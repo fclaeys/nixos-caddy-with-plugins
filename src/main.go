7a5478133f132bd46c5b4eb3f9d902bfde31c59c
Fabrice Claeys <fabrice.claeys@symbol-it.fr>
2024-06-06 14:16:55 +0200

    revert add l4 plugins

--- src/main.go
+++ src/main.go
@@ -16,7 +16,7 @@ import (
 	// plug in Caddy modules here
 	_ "github.com/caddyserver/caddy/v2/modules/standard"
 	_ "github.com/caddy-dns/cloudflare"
-  _ "github.com/mholt/caddy-l4"
+  // _ "github.com/mholt/caddy-l4"
 )
 
 func main() {
